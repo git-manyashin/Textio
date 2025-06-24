295ce084da80e1a84c658be127386f6bfb540766
git-manyashin <johnny.work.development@gmail.com>
2025-06-24 21:04:11 +0200

    basic stuff, types,variables,numbers,increment, decrement

---
+++ CH1_variables/L6_numbers_in_js/main.js
@@ -0,0 +1,15 @@
+const promoMessages = 12;
+const reminderMessages = 15;
+const welcomeMessages = 8;
+const supportMessages = 5;
+
+
+
+const totalMessagesSent =
+  promoMessages + reminderMessages + welcomeMessages + supportMessages;
+const averageMessagesSent = totalMessagesSent / 4;
+
+
+
+console.log("Total messages:", totalMessagesSent);
+console.log("Average messages:", averageMessagesSent);
---
+++ CH1_variables/L8_increment_and_decrement/main.js
@@ -0,0 +1,7 @@
+let numFailedMessages = 1336;
+
+numFailedMessages++;
+
+// or numFailedMessages++; numFailedMessages += 5; numFailedMessages -= 5;
+
+console.log(numFailedMessages + " failed messages");
